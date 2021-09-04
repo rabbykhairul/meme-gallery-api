23ce0521f113b9c4fb70aee8bd42f14f8a49f970
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 01:29:22 +0600

    create basic server skeleton

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// =====
+// connect to database
+// =====
+mongoose.connect(process.env.mongoDBConnectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Established Mongoose Connection");
+});
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
