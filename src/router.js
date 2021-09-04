efe0a88eb98a0b828e01710241a18ac42d1a1d6a
rabbykhairul <rabbykhairul@gmail.com>
2021-09-05 04:17:52 +0600

    router.js - transfer all requests coming to '/memes' to memeRoutes.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const memeRoutes = require("./routes/memeRoutes");
 
 router.get("/", (req, res) => res.send("<h1>Hello from meme-gallery-api</h1>"));
 
+router.use("/memes", memeRoutes);
 
 module.exports = router;
